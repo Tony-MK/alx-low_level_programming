b4a4284995dbcd9618a8c6b6ec9beb9e4639c4c6
Antony Kithinzi <kithinzi97@gmail.com>
2022-03-11 15:46:59 -0800

    Added last task fiel

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ *  * main - Prints all possible double digits integers combinations pairs
+ *  *  * Return: 0 Always (sucess)
+ */
+int main(void)
+{
+	short int j, k, m;
+	short int i = 48;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			k = i;
+			while (k < 58)
+			{
+				m = j;
+				while (m < 57)
+				{
+					m++;
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(m);
+
+					if (i != 57 || j != 56 || k != 57 || m != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+
+				k++;
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
