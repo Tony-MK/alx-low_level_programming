0137437472219efd8c72b00fed1f5cb012cef8cb
Antony Kithinzi <kithinzi97@gmail.com>
2022-03-16 14:18:19 -0700

    Modified 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,42 +1,45 @@
-
 #include "main.h"
 /**
 * * print_to_98 - Prints on numbers until 98
-* * * @n: Number to start int to add
+* * * @n: Numbers to start printing from
 */
 void print_to_98(int n)
 {
-	int i, p;
+	register int p, t;
 
-	while (n < 99)
+	(n < 98 ? n-- : n++);
+	while (n != 98)
 	{
-		i = n;
-		if (i < 0)
+		(n < 98 ? n++ : n--);
+		if (n == 0)
 		{
-			_putchar('-');
-			i = -i;
+			_putchar('0');
 		}
-		p = 1;
-		while (!(p > n))
+		else
 		{
-			p *= 10;
-		}
-		while (p >= 10)
-		{
-			p /= 10;
-			_putchar((i > 9 ? ((i / p) % 10) : i) + '0');
-		}
+			p = 1;
+			t = n;
+			if (t < 0)
+			{
+				_putchar('-');
+				t = -t;
+			}
+			while (p <= t)
+				p *= 10;
+			if (p > t)
+				p /= 10;
 
-		if (n < 98)
+			while (p > 0)
+			{
+				_putchar((t / p) % 10 + '0');
+				p /= 10;
+			}
+		};
+		if (n != 98)
 		{
 			_putchar(' ');
 			_putchar(',');
 		}
-		else
-		{
-			_putchar('\n');
-		}
-		n++;
 	}
-
+	_putchar('\n');
 }
