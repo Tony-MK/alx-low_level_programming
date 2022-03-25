deca7d024cb0d0db9c0dcfa29fffcff5b6ebe77d
Antony Kithinzi <kithinzi97@gmail.com>
2022-03-25 13:47:03 -0700

    Modified 101.print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,30 +5,13 @@
 */
 void print_number(int n)
 {
-	register unsigned int p, t;
 
-	if (n == 0)
+	if (n < 0)
 	{
-		_putchar('0');
+		_putchar('-');
+		n = -n;
 	}
-	else
-	{
-		p = 1;
-		t = n;
-		if (n < 0)
-		{
-			_putchar('-');
-			t = -n;
-		}
-		while (p <= t)
-			p *= 10;
-		if (p > t)
-			p /= 10;
-
-		while (p > 0)
-		{
-			_putchar((t / p) % 10 + '0');
-			p /= 10;
-		}
-	};
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
 }
