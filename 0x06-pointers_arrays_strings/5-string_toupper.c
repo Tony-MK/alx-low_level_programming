c01684cbe812036463f234d872a144db6edfb69e
Antony Kithinzi <kithinzi97@gmail.com>
2022-03-25 10:40:09 -0700

    Modified string-tupper file

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,15 @@
 /**
-* * strcat - Concatenates two strings
-* * * @dest: First string
-* * * * @src: Other string
-* * * * * Return: Char pointer to concatenated string
+* * string_toupper - Capitalizes string
+* * * @s: String to capitalize
+* * * * Return: Char pointer to capatilized string
 */
 char *string_toupper(char *s)
 {
 	register unsigned short int i = 0;
-	
+
 	while (*(s + i) != '\0')
 	{
-		if (*(s + i) > 97 && *(s + i) < 123)
+		if (*(s + i) > 96 && *(s + i) < 123)
 		{
 			*(s + i) -= 32;
 		}
