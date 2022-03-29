670759cabb31ad5cea998468ebc06b2f3a1a14e6
Antony Kithinzi <kithinzi97@gmail.com>
2022-03-28 18:13:58 -0700

    added strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,31 +1,30 @@
 #include <stddef.h>
 /**
  * * _strstr - Locates a substring
- * * * @haystack: String to search 
+ * * * @haystack: String to search
  * * * * @needle: Substring to locate
  * * * * * Return: Pointer to the cahr of string at first occurence of haystack
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i = 0, h = 0, n;
+
 	if (*(needle + 0) == '\0')
 		return (haystack);
-
-	register unsigned int h = 0, n, i;
-
 check:
 	n = 0;
 	i = h;
-	while (*(haystask + i) != 0)
+	while (*(haystack + i) != 0)
 	{
-		if ((haystack + i) != *(needle + n))
+		if (*(haystack + i) != *(needle + n))
 		{
-			i++;
+			h++;
 			goto check;
 		}
-		
 		n++;
+		i++;
 		if (*(needle + n) == 0)
-			return (haystask + h);
+			return (haystack + h);
 	}
 	return (NULL);
 }
