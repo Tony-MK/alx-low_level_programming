5198db2900056821926253f7a479b1ee0784ca1c
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-01 13:53:20 -0700

    Changed task 1 file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,9 @@
  * @argv : String arguments
  * Return: Always 0
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char **argv __attribute__((unused)))
 {
-	print_int(argc);
+	print_int(argc - 1);
 	_putchar(0x0A);
 	return (0);
 }
