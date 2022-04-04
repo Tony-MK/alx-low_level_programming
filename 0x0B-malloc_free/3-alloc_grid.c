db34353250bcd83469a9af2bb117b084d2c9d523
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-04 12:22:36 -0700

    Added 3-alloc_grid.c file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - Initializes an array with a specific char
+ * @width: The size of the array
+ * @height: Character to initialize with
+ * Return: A pionter to a char. Otherwise, NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array = NULL;
+
+	if (width == 0 || height == 0)
+		return (array);
+	array = malloc(sizeof(int *) * height);
+set:
+	height--;
+	*(array + height) = malloc(sizeof(int) * width);
+	if (*(array + height) == NULL)
+		return (NULL);
+	else if (height == 0)
+		return (array);
+	goto set;
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,4 +2,5 @@
 #define __MAIN_H__
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+int **alloc_grid(int width, int height);
 #endif
