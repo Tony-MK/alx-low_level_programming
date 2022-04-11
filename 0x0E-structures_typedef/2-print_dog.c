e2d441d989cd418d090f400f5cd93d9a661c9835
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-11 16:29:49 -0700

    changed task files

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,14 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: ");
-		(d->name == NULL ? printf("(nil)") : printf("%s", d->name));
+		if (d->name == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", d->name);
+		}
 
 		printf("\nAge: %f", d->age);
 
