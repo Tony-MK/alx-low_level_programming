03218edfcc2769b7a8ea5b9af1d0e88b49c297bc
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-11 17:17:02 -0700

    changed 4-new_dog.c file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,26 +13,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog = malloc(sizeof(dog_t));
 	char *ptr;
 
+	if (dog == NULL)
+		return (NULL);
 	dog->name = malloc(sizeof(*name));
 	if (dog->name == NULL)
+	{
+		free(dog);
 		return (NULL);
-
+	}
 	dog->owner = malloc(sizeof(*owner));
 	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
 		return (NULL);
-
+	}
 	*(dog->name + (sizeof(*name) - 1)) = '\0';
 	*(dog->owner + (sizeof(*owner) - 1)) = '\0';
 	dog->age = age;
 	ptr = dog->name;
-
 	while (*name != '\0')
 	{
 		*ptr = *name;
 		ptr++;
 		name++;
 	}
-
 	ptr = dog->owner;
 	while (*owner != '\0')
 	{
@@ -40,6 +45,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 		ptr++;
 		owner++;
 	}
-
 	return (dog);
 }
