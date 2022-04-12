10f44626a6fcb3d28c00a4edabae4348b30b8ab1
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-11 17:12:52 -0700

    changed task files

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include "dog.h"
-/*
+/**
  * init_dog - Initializes a dog's attributes
- * @dog: The dog's memory address
+ * @d: The dog's memory address
  * @name: The dog's name as a string
  * @age: The dog's age as an integer
  * @owner: The dog's owner's name as a a string
