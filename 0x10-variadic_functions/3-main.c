d37084f41cde1b7a0c6582ac4cbf7207a3818b95
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-14 19:32:39 -0700

    Modified 3-print_all.c

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("ceissf", 'B', 3, "stSchool", NULL, .3);
+    print_all(NULL);
+    return (0);
+}
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,50 +9,41 @@
  */
 void print_all(const char * const format, ...)
 {
-	register unsigned long int i = 0;
-	register char c = 0;
+	register unsigned long int i = 0, c = 0;
+	char *string, *s = "%d";
 	va_list strings;
-	char *string;
-	
-	if (format == NULL)
-	{
-		printf("\n");
-		return;
-	}
 
-	va_start(strings, format);
-	while (*(format + i))
+	if (format != NULL)
 	{
-		if ((c == 1) && (*(format + i) == 's' || *(format + i) == 'i' ||
-			*(format + i) == 'f' || *(format + i) == 'c'))
-			printf(", ");
-		switch (*(format + i++))
+		va_start(strings, format);
+		while (*(format + i))
 		{
-		case 's':
-			c = 1;
-			string = va_arg(strings, char *);
-			if (string)
+			if ((c == 1) && (*(format + i) == 's' || *(format + i) == 'i' ||
+				*(format + i) == 'f' || *(format + i) == 'c'))
+				printf(", ");
+			switch (*(format + i))
 			{
-				printf("%s", string);
+			case 's':
+				c = 1;
+				string = va_arg(strings, char *);
+				if (string)
+					s = "%s";
+				printf(s, string);
+				s = "%p";
 				break;
-			}
-			printf("%p", string);
-			break;
-
-		case 'i':
-			c = 1;
-			printf("%d", va_arg(strings, int));
-			break;
-
-		case 'f':
-			c = 1;
-			printf("%f", va_arg(strings, double));
-			break;
-
-		case 'c':
-			c = 1;
-			printf("%c", va_arg(strings, int));
-			break;
+			case 'i':
+				c = 1;
+				printf("%d", va_arg(strings, int));
+				break;
+			case 'f':
+				c = 1;
+				printf("%f", va_arg(strings, double));
+				break;
+			case 'c':
+				c = 1;
+				printf("%c", va_arg(strings, int));
+				break; }
+			i++;
 		}
 	}
 	va_end(strings);
