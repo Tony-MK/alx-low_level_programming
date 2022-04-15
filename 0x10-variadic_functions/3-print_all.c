cdc23b61c95381fae1427d7c3a7bc422a888c1ca
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-14 19:37:22 -0700

    Modified 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,38 +13,36 @@ void print_all(const char * const format, ...)
 	char *string, *s = "%d";
 	va_list strings;
 
-	if (format != NULL)
+	va_start(strings, format);
+	while (*(format + i))
 	{
-		va_start(strings, format);
-		while (*(format + i))
+		if ((c == 1) && (*(format + i) == 's' || *(format + i) == 'i' ||
+			*(format + i) == 'f' || *(format + i) == 'c'))
+			printf(", ");
+		switch (*(format + i))
 		{
-			if ((c == 1) && (*(format + i) == 's' || *(format + i) == 'i' ||
-				*(format + i) == 'f' || *(format + i) == 'c'))
-				printf(", ");
-			switch (*(format + i))
-			{
-			case 's':
-				c = 1;
-				string = va_arg(strings, char *);
-				if (string)
-					s = "%s";
-				printf(s, string);
-				s = "%p";
-				break;
-			case 'i':
-				c = 1;
-				printf("%d", va_arg(strings, int));
-				break;
-			case 'f':
-				c = 1;
-				printf("%f", va_arg(strings, double));
-				break;
-			case 'c':
-				c = 1;
-				printf("%c", va_arg(strings, int));
-				break; }
-			i++;
+		case 's':
+			c = 1;
+			string = va_arg(strings, char *);
+			if (string)
+				s = "%s";
+			printf(s, string);
+			s = "%p";
+			break;
+		case 'i':
+			c = 1;
+			printf("%d", va_arg(strings, int));
+			break;
+		case 'f':
+			c = 1;
+			printf("%f", va_arg(strings, double));
+			break;
+		case 'c':
+			c = 1;
+			printf("%c", va_arg(strings, int));
+			break;
 		}
+		i++;
 	}
 	va_end(strings);
 	printf("\n");
