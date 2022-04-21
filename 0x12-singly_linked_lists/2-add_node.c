6c14b693493c2ca2aa300137030124954642b8af
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-21 16:24:31 -0700

    added 2-add_node.c file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,42 +1,25 @@
-#include<stddef.h>
+#include<stdlib.h>
+#include<string.h>
 #include "lists.h"
 /**
  * add_node - Adds a node to a singly linked list
- * @h: Head of a singly linked list
+ * @head: Head of a singly linked list
  * @str: Node string value
+ * Return: Pointer to the adderss of the new element. Otherwise, 0.
  */
-list_t add_node(const list_t *h, char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	register node *list_t;
+	register list_t *node;
 
 	node = malloc(sizeof(list_t));
 	if (!(node))
 		return (NULL);
 
+	(*node).next = (*head);
 	(*node).str = strdup(str);
-	if ((*node).str == NULL && str != NULL)
-	{
-		free(node);
-		return (NULL);
-	}
-
-transverse:
-
-	if (!(h))
-		h = node;
-	else if ((*h).next)
-	{
-		h = h->next;
-		goto transverse;
-	}
-	else
-	{
-		(*h).next = node;
-	}
-
 	(*node).len = 0x0;
 	while (*(str + (*node).len))
 		(*node).len++;
-
+	(*head) = node;
 	return (node);
 }
