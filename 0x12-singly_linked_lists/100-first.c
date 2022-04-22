e8e5d999543277034c14872ee95194a07c9a1404
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-21 17:08:29 -0700

    Changed 100-first.c file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
 /**
  * first - Prints a sentence befere the main function call
  */
+
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
