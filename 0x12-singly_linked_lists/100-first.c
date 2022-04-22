1cc141ee1df100975c8392f8b2a42944f9d09240
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-21 17:06:22 -0700

    Added first advanced tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * first - Prints a sentence befere the main function call
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
