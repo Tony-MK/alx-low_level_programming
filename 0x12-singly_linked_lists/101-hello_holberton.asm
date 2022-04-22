20a9eb33becdfdce47643f912e11563728704952
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-21 18:14:14 -0700

    added last task file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,10 +2,16 @@
 	extern printf
 
 section .data:
-	string: db "Hello, Holberton", 0x0A, 0
+
+string: db "Hello, Holberton", 0x0A, 0
+
+section .text:
+
 main:
 	mov	edi, string
 	xor	eax, eax
 	call printf
-	mov	eax, 0
+	mov	eax, 0x1
+	mov	ebx, 0
+	int 0x80
 	ret
