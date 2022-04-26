0220c6f2e7ac241d7d5c1fff1130308dec218cd9
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-26 05:49:04 -0700

    Added 5-free_listint2.c task file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,11 +4,21 @@
  * free_listint2 - Frees a singly linked list from memory
  * @head: Head of the singly linked list
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
+	register listint_t *tmp, *tmp2;
+
 	if (head == NULL)
 		return;
-	else if ((*head).next)
-		free_listint((*head).next);
-	free(head);
+
+	tmp = (*head)->next;
+	while (tmp)
+	{
+		tmp2 = (*tmp).next;
+		free(tmp);
+		tmp = tmp2;
+	}
+	free(*head);
+
+	(*head) = NULL;
 }
---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - Removes the head element in a singly linked list
+ * @head: Head of the singly linked list
+ * Return: Value of head node data
+ */
+int pop_listint(listint_t *head)
+{
+	register listint_t *tmp;
+	register int n;
+
+	if (head == NULL)
+		return 0;
+	n = head->n;
+	tmp = head;
+	head = (*head).next;
+	free(tmp);
+	return (n);
+}
