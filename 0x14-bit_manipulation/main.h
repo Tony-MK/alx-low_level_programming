682fb080b8ed70ee8985ca107f0538e0d45a17f8
Antony Kithinzi <kithinzi97@gmail.com>
2022-04-28 06:19:43 -0700

    Created main header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef ___MAIN_H__
+#define ___MAIN_H__
+
+unsigned int binary_to_unit(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
