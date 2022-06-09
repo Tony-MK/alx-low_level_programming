f21ff8ef0b6c44901e247731330174271d12eff6
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-09 16:17:28 -0700

    added palindrome task files

--- 0x17-doubly_linked_lists/102-palindrome.py
+++ 0x17-doubly_linked_lists/102-palindrome.py
@@ -1,8 +1,9 @@
 #!usr/bin/python3
 
-
+largest_palindrome = 0;
 for i in range(999, 100, -1):
     for k in range(999, 100, -1):
-        if str(i * k) == str(i * k)[::-1]:
-            print("{} * {} = {}".format(i, k, i * k))
-            exit(1)
+        if i * k > largest_palindrome and str(i * k) == str(i * k)[::-1]:
+            largest_palindrome = i * k
+            print("{} * {} = {}".format(i, k, largest_palindrome))
+            
