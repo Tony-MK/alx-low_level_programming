7deb61c8c9a9832075708a4738ae6fae4d719451
Antony Kithinzi <kithinzi97@gmail.com>
2022-06-09 11:45:14 -0700

    added task 4 files

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees doubly linked list from memory
+ * @head: Pointer to the head of the linked list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
